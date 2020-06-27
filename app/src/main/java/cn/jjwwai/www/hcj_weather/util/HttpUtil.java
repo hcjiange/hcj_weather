36f77163093d30d044716bf637808d69965c4dde
hcjiange <2251641778@qq.com>
2020-06-27 21:12:00 +0800

    First Commit!

--- app/src/main/java/cn/jjwwai/www/hcj_weather/util/HttpUtil.java
+++ app/src/main/java/cn/jjwwai/www/hcj_weather/util/HttpUtil.java
@@ -1,8 +1,17 @@
 package cn.jjwwai.www.hcj_weather.util;
 
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Created by jiange on 2020/6/27.
  */
 
-public class HttpUtil {
+public class HttpUtil {//发送api请求基础类
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
 }
